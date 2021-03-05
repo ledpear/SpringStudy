63c493d3f4a30dde4db0b5ef2e1093538d79c749
ledpear <tjsrb75@gmail.com>
2021-03-05 21:18:45 +0900

    스프링 웹 개발 기초
    1. 템플릿 엔진을 사용하여 콘트롤러에서 뷰로 데이터 전달 실습
    2. API 함수를 구현하여 객체를 리턴(Json으로)

--- hello-spring/src/main/java/ledpear/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/ledpear/hellospring/controller/HelloController.java
@@ -3,13 +3,15 @@ package ledpear.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data", "hello!!");
+    public String hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("data", name);
         return "hello";
     }
 
@@ -18,5 +20,32 @@ public class HelloController {
         model.addAttribute("data", "helloooooooooo");
         model.addAttribute("data2", "helloooooooooo2");
         return "hello2";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public  String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+        private char test;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
