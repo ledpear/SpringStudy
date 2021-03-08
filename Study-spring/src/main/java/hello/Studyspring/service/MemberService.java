8e5ef052d99b4dc23110c134818191e8776f459e
ledpear <tjsrb75@gmail.com>
2021-03-09 00:28:09 +0900

    회원 관리 예제 - 백엔드 개발2
    1. 회원 리포지토리 테스트 완료
    2. 회원 서비스 구현

---
+++ Study-spring/src/main/java/hello/Studyspring/service/MemberService.java
@@ -0,0 +1,41 @@
+package hello.Studyspring.service;
+
+import hello.Studyspring.domain.Member;
+import hello.Studyspring.repository.MemberRepository;
+import hello.Studyspring.repository.MemoryMemberRepository;
+
+import java.util.IllformedLocaleException;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원은 등록할 수 없다
+        validateDuplicateMember(member); //중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
--- Study-spring/src/test/java/hello/Studyspring/repository/MemoryMemberRepositoryTest.java
+++ Study-spring/src/test/java/hello/Studyspring/repository/MemoryMemberRepositoryTest.java
@@ -3,13 +3,21 @@ package hello.Studyspring.repository;
 import hello.Studyspring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ObjectAssert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -34,6 +42,21 @@ class MemoryMemberRepositoryTest {
         
         Member result = repository.findByName("spring1").get();
 
-        assertThat(result).isEqualTo(member1)
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring2");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 }
