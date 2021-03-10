df68d9e9e17c0c88a4eeb16806f7d5044a4d2d7f
ledpear <tjsrb75@gmail.com>
2021-03-11 01:16:45 +0900

    회원관리 웹 MVC 개발 - 등록 화면 추가 및 기능 추가

--- Study-spring/src/main/java/hello/Studyspring/controller/MemberController.java
+++ Study-spring/src/main/java/hello/Studyspring/controller/MemberController.java
@@ -1,9 +1,11 @@
 package hello.Studyspring.controller;
 
+import hello.Studyspring.domain.Member;
 import hello.Studyspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -21,4 +23,18 @@ public class MemberController {
 //        this.memberService = memberService;
 //    }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+       Member member = new Member();
+       member.setName(form.getName());
+
+       memberService.join(member);
+
+       return "redirect:/";
+    }
 }
