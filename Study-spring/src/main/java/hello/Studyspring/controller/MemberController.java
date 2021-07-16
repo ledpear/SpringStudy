6c78f497417a2894060ca1ed746a0c9192161bc3
ledpear <tjsrb75@gmail.com>
2021-07-16 16:18:26 +0900

    회원관리 웹 MVC 개발 - 회원 조회 기능 추가

--- Study-spring/src/main/java/hello/Studyspring/controller/MemberController.java
+++ Study-spring/src/main/java/hello/Studyspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.Studyspring.domain.Member;
 import hello.Studyspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -24,7 +27,8 @@ public class MemberController {
 //    }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm(Model model){
+        model.addAttribute("data", memberService.findMembers());
         return "members/createMemberForm";
     }
 
@@ -36,5 +40,12 @@ public class MemberController {
        memberService.join(member);
 
        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public  String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
